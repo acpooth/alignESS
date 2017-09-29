4474b8d8c9bcb188c4e8d126b8ec2c0b8d8a68ba
Augusto Cesar Poot-Hernandez <dragopoot@gmail.com>
2017-09-29 16:48:10 -0500

    Folder cleaning and removing legacy scripts

--- tests/test_nwx.py
+++ tests/test_nwx.py
@@ -64,6 +64,30 @@ def test_scoring(seqs, seqs_s, decs, lecs, hmat):
         assert abs(sco - scos) <= 1e-6
 
 
+def test_NW_score(hmat, decs, lecs):
+    """Test for similarity in old and new alignment scoring
+    Complete NW version
+    """
+    s1 = '6.2.1:2.3.1:1.2.4:2.7.1:4.2.1:5.4.2:2.7.2:1.2.1:4.1.2:2.7.1:5.3.1:5.4.2:3.1.3'
+    s1_ = '2.3.1:1.2.4:2.7.1:5.4.2:2.7.2:1.2.1'
+    # s2 = '1.2.4:2.3.1:2.3.3:4.1.1'
+    # s3 = '6.3.3:6.3.4:5.4.99:6.3.2:4.3.2:2.4.2:3.2.2'
+    ss1 = s1.split(':')
+    ss1_ = s1_.split(':')
+    # ss2 = s2.split(':')
+    # ss3 = s3.split(':')
+    ess = [ss1, ss1_]
+    for ii, i in enumerate(ess):
+        for ji, j in enumerate(ess):
+            score_old = nw.NW(hmat, lecs, ':'.join(i), ':'.join(j))[2]
+            score_new = nwx.NW(hmat, decs, i, j)[2]
+            assert abs(score_old - score_new) <= 1e-2
+            score_old_loc = nw.NW(hmat, lecs, ':'.join(i), ':'.join(j),
+                                  local=True, localize=True)[2]
+            score_new_loc = nwx.NW(hmat, decs, i, j, localize=True)[2]
+            assert abs(score_old_loc - score_new_loc) <= 1e-2
+
+
 def test_indvsalldb_len(seqs, decs, hmat):
     """Test for the correct length of ind_vs_alldb
 
